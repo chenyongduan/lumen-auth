1f8f7f65cdac81e3f280c43b4388bafa75cae208
chenyongduan <chenyongduan@97kid.com>
2018-04-27 18:58:20 +0800

    feat(*)images数据表结果修改；

--- database/migrations/2018_04_27_182403_create_images_table.php
+++ database/migrations/2018_04_27_182403_create_images_table.php
@@ -15,6 +15,7 @@ class CreateImagesTable extends Migration
     {
         Schema::create('images', function (Blueprint $table) {
             $table->increments('id');
+            $table->integer('admin_id');
             $table->string('image_name');
             $table->timestamps();
         });
